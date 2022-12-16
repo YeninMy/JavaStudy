ea62c0a48183b4dc215a7f0cdc602876cc52b4b4
YeninMy <vbif555666@gmail.com>
2022-12-16 02:13:47 +0200

    Homework 6. Fixes

---
+++ yenin/src/onlineSchool/сources/cource.java
@@ -0,0 +1,33 @@
+package onlineSchool.сources;
+
+public class cource {
+    public int courceId;
+
+    public String name;
+
+    public static int engLecCounter;
+
+    public static int gerLecCounter;
+
+    public static int frLecCounter;
+
+    public static int courceCounter;
+
+    String topic;
+
+    public cource(String name, int courceId, String topic) {
+        this.name = name;
+        this.courceId = courceId;
+        courceCounter++;
+        lecture.lecCounter++;
+        this.topic = topic;
+        if (topic == "English") {
+            engLecCounter++;
+        } else if (topic == "German") {
+            gerLecCounter++;
+        } else if (topic == "French") {
+            frLecCounter++;
+        }
+    }
+}
+
