71b4156414fed13fb290a9da8f23dd622739797b
YeninMy <vbif555666@gmail.com>
2023-02-15 15:14:06 +0200

    hw28.3

--- yenin/src/hw28/Main.java
+++ yenin/src/hw28/Main.java
@@ -11,14 +11,14 @@ public class Main {
         Logger.createFile(Logger.pathLogs);
 
         List<Matarials> m1 = new ArrayList<>();
-        m1.add(new Matarials(0,Type.VIDEO));
-        m1.add(new Matarials(1,Type.URL));
-        m1.add(new Matarials(2,Type.BOOK));
+        m1.add(new Matarials(0, Type.VIDEO));
+        m1.add(new Matarials(1, Type.URL));
+        m1.add(new Matarials(2, Type.BOOK));
         List<Matarials> m2 = new ArrayList<>();
-        m2.add(new Matarials(3,Type.VIDEO));
-        m2.add(new Matarials(4,Type.URL));
+        m2.add(new Matarials(3, Type.VIDEO));
+        m2.add(new Matarials(4, Type.URL));
         List<Matarials> m3 = new ArrayList<>();
-        m1.add(new Matarials(5,Type.VIDEO));
+        m1.add(new Matarials(5, Type.VIDEO));
 
 
         Lecture l1 = new Lecture(1, "Arrays",
@@ -34,14 +34,16 @@ public class Main {
         l3.setMaterials(m1);
         Lecture.lectures.add(l3);
 
-        Person p1 = new Person(1, "Vlad", "Bedrytskiy","vlad@gmail.com");
+        Person p1 = new Person(1, "Vlad", "Bedrytskiy", "vlad@gmail.com");
         Person.persons.add(p1);
         Person p2 = new Person(2, "Bogdan", "Shelest", "shelest@gmail.com");
         Person.persons.add(p2);
         Person p3 = new Person(3, "Vlad", "Zhuravel", "vlad@gmail.com");
 
         p1.setEmail("q1@gmail.com");
-        p1.setEmail("q1@gmail.com");
+        p1.setEmail("q2@gmail.com");
+        p2.setEmail("q3@gmail.com");
+
 
         Optional<Lecture> min = Lecture.lectures.stream()
                 .min(Comparator.comparing(Lecture::getLocalDate));
@@ -54,8 +56,9 @@ public class Main {
         System.out.println("Lecture with the most additional materials:");
         System.out.println(max);
 
-
+        long half = (Files.lines(Paths.get(Logger.pathLogs)).count()) / 2;
         System.out.println("Quantity of INFO: " + Files.lines(Paths.get(Logger.pathLogs))
+                .skip(half)
                 .filter(s -> s.contains("INFO"))
                 .count());
     }
