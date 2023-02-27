e86e69759b75a8b8a189dc6da201defb25a2ceec
YeninMy <vbif555666@gmail.com>
2023-02-27 22:37:13 +0200

    maven

---
+++ yenin/src/hw31/Lecture.java
@@ -0,0 +1,11 @@
+package hw31;
+
+public class Lecture {
+private int id;
+private String name;
+
+    public Lecture(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
---
+++ yenin/src/hw31/Main.java
@@ -0,0 +1,6 @@
+package hw31;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
